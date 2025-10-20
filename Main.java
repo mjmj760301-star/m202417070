bffb8fabfa140f23b7014bdf6b2928c2fcde8dad
aaa <aa@a.com>
2025-10-20 17:15:49 +0900

    five

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package m202417070;
 public class Main {public static void main(String[] args) {
     System.out.println("박규민");
     World.run();
-    Hello.run();
+    System.out.println("안녕하세요");
   }
     }
